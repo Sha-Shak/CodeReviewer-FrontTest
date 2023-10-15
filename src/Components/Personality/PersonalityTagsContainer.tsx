b900b6d430f73a5a4ac68ddee74f7c42ec7bb851
Samiya Kazi <samiya.kazi09@gmail.com>
2023-10-15 18:16:26 +0600

    feat: added edit ability for instrutor submission

--- src/Components/Personality/PersonalityTagsContainer.tsx
+++ src/Components/Personality/PersonalityTagsContainer.tsx
@@ -11,8 +11,8 @@ import { PlusOutlined } from "@ant-design/icons";
 function PersonalityTagsContainer({ id }: { id: string }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [studentTags, setStudentTags] = useState<IPersonalityTagCount[]>([]);
-  const [personalityTags, setPersonalityTags] = useState<IPersonalityTag[]>([]);
-  const [skillTags, setSkillTags] = useState<IPersonalityTag[]>([]);
+  const [allTags, setAllTags] = useState<IPersonalityTag[]>([]);
+  const [instructorTags, setInstructorTags] = useState<IPersonalityTag[]>([]);
   const [selectedType, setSelectedType] = useState<string>("personality");
 
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
@@ -20,27 +20,36 @@ function PersonalityTagsContainer({ id }: { id: string }) {
   const [loading, setLoading] = useState<boolean>(false);
   const [modalLoading, setModalLoading] = useState<boolean>(false);
 
-  const tagOptions = [{ label: "Personality", value: "personality" }, { label: "Skill", value: "skill" }]
-
   useEffect(() => {
-    async function fetchTags() {
-      try {
-        setModalLoading(true);
-        const tagsUrl = `${conf.API_BASE_URL}/personality/all`;
-        const allTags: IPersonalityTag[] = await serverFetch("get", tagsUrl);
-
-        setPersonalityTags(allTags.filter(tag => tag.type === "personality"));
-        setSkillTags(allTags.filter(tag => tag.type === "skill"));
-        setModalLoading(false);
-      } catch (error) {
-        setModalLoading(false);
-        console.log(error);
-      }
-    }
-
     fetchTags();
     fetchStudentTags();
   }, [])
+
+  async function fetchTags() {
+    try {
+      setModalLoading(true);
+      const tagsUrl = `${conf.API_BASE_URL}/personality/all`;
+      const allTags: IPersonalityTag[] = await serverFetch("get", tagsUrl);
+      setAllTags(allTags);
+      await fetchInstructorGivenTags();
+      setModalLoading(false);
+    } catch (error) {
+      setModalLoading(false);
+      console.log(error);
+    }
+  }
+
+
+  async function fetchInstructorGivenTags() {
+    try {
+      const url = `${conf.API_BASE_URL}/personality/instructor/${id}`;
+      const tags : IPersonalityTag[] = await serverFetch("get", url);
+      setInstructorTags(tags);
+    } catch (error) {
+      console.log(error);
+      throw new Error('error in fetching instructor tags.');
+    }
+  }
 
 
   async function fetchStudentTags() {
@@ -70,13 +79,14 @@ function PersonalityTagsContainer({ id }: { id: string }) {
 
   const showModal = () => {
     setIsModalOpen(true);
+    fetchTags();
   };
 
   const handleOk = async () => {
     try {
       setIsModalOpen(false);
       const body = {reviewerType: "instructor", tags: selectedTags};
-      const url = `${conf.API_BASE_URL}/personality/submission/${id}`;
+      const url = `${conf.API_BASE_URL}/personality/instructor/${id}`;
       await serverFetch('post', url, body);
       await fetchStudentTags();
     } catch (error) {
@@ -101,20 +111,14 @@ function PersonalityTagsContainer({ id }: { id: string }) {
       <Button type="primary" onClick={showModal} size="large" icon={<PlusOutlined />}>Add tags</Button>
       <Spin spinning={modalLoading} tip="Fetching personality tags...">
         <Modal title="Add Tags" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-          <Radio.Group
-            onChange={handleTypeChange}
-            options={tagOptions}
-            optionType="button"
-            buttonStyle="solid"
-            defaultValue="personality"
-          />
           <Select
             mode="tags"
             size="middle"
             placeholder="Select tags"
             onChange={handleChange}
             style={{ width: '100%', marginTop: 20 }}
-            options={(selectedType === "personality" ? personalityTags : skillTags).map((tag) => ({ label: parseName(tag.name), value: tag._id }))}
+            defaultValue={instructorTags.map(tag => tag._id)}
+            options={allTags.map((tag) => ({ label: parseName(tag.name), value: tag._id }))}
           />
         </Modal>
       </Spin>
