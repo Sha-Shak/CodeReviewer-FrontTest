37e8a3008998eab047cbad5b76a8ac7cdb22553b
sha-shak <shariar.akash@northsouth.edu>
2023-10-23 00:17:52 +0600

    add: skeleton

--- src/Components/Charts/Prospect/ProspectRadarChart.tsx
+++ src/Components/Charts/Prospect/ProspectRadarChart.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
+import { Skeleton } from "antd";
+import { useEffect, useState } from "react";
 import {
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
   Radar,
   RadarChart,
-  PolarGrid,
-  PolarAngleAxis,
-  PolarRadiusAxis,
   ResponsiveContainer,
   Tooltip,
 } from "recharts";
-import { Spin } from "antd";
-import CustomRadarTooltip from "../Tooltips/CustomRadarTooltip";
 import { serverFetch } from "../../../utils/handleRequest";
+import CustomRadarTooltip from "../Tooltips/CustomRadarTooltip";
 
-function RadarChartComponent({ url, title }: {url: string, title: string}) {
+function RadarChartComponent({ url, title }: { url: string; title: string }) {
   const [loading, setLoading] = useState(false);
   const [report, setReport] = useState([]);
 
@@ -40,35 +40,40 @@ function RadarChartComponent({ url, title }: {url: string, title: string}) {
   return (
     <div className="chart-container text-center mb-1">
       <h2 className="chart-title">{title}</h2>
-      <Spin spinning={loading} tip="Fetching data" size="large">
-        <div style={{ width: "100%", height: "200px" }}>
-          {report && report.length ? (
-            <ResponsiveContainer>
-              <RadarChart cx="50%" cy="50%" outerRadius="80%" data={report}>
-                <PolarGrid />
-                <PolarAngleAxis
-                  dataKey="skillName"
-                  tick={{ fontSize: "small" }}
-                />
-                <PolarRadiusAxis domain={[0, 10]} />
-                <Tooltip
-                  cursor={{ strokeDasharray: "3 3" }}
-                  content={<CustomRadarTooltip />}
-                />
-                <Radar
-                  name="Marks"
-                  dataKey="averageMarks"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                  fillOpacity={0.6}
-                />
-              </RadarChart>
-            </ResponsiveContainer>
-          ) : (
-            <p>No marks for {title}, yet.</p>
-          )}
-        </div>
-      </Spin>
+
+      <div style={{ width: "100%", height: "200px" }}>
+        {loading ? (
+          <Skeleton.Avatar size={164} active/>
+        ) : (
+          <>
+            {report && report.length ? (
+              <ResponsiveContainer>
+                <RadarChart cx="50%" cy="50%" outerRadius="80%" data={report}>
+                  <PolarGrid />
+                  <PolarAngleAxis
+                    dataKey="skillName"
+                    tick={{ fontSize: "small" }}
+                  />
+                  <PolarRadiusAxis domain={[0, 10]} />
+                  <Tooltip
+                    cursor={{ strokeDasharray: "3 3" }}
+                    content={<CustomRadarTooltip />}
+                  />
+                  <Radar
+                    name="Marks"
+                    dataKey="averageMarks"
+                    stroke="#8884d8"
+                    fill="#8884d8"
+                    fillOpacity={0.6}
+                  />
+                </RadarChart>
+              </ResponsiveContainer>
+            ) : (
+              <p>No marks for {title}, yet.</p>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- src/Components/ProfileBadge.tsx
+++ src/Components/ProfileBadge.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import avatar from '../assets/defaultavatar.png'
-import { IProspect } from '../interfaces/prospects/prospects.interface'
-import { Col, Row, Space } from 'antd'
+import { Col, Row, Skeleton, Space } from "antd";
+import avatar from "../assets/defaultavatar.png";
+import { IProspect } from "../interfaces/prospects/prospects.interface";
 
-const ProfileBadge = ({profile}: {profile: IProspect}) => {
-  console.log("first", profile)
-  const handleStage = ()=>{
+const ProfileBadge = ({ profile }: { profile: IProspect }) => {
+  console.log("first", profile);
+  const handleStage = () => {
     switch (profile.stage) {
       case "motivational-interview":
         return "Motivational Interview";
@@ -16,23 +15,27 @@ const ProfileBadge = ({profile}: {profile: IProspect}) => {
       default:
         break;
     }
-  }
-  
+  };
+
   return (
     <div style={{ border: "1px solid #d9d9d9", padding: "15px" }}>
-      <Space align="center">
-        <Row gutter={100}>
-          <Col span={6}>
-            <img className="w-24" src={avatar} alt="avatar" />
-          </Col>
-          <Col span={18}>
-            <h2>{profile.first_name + " " + profile.last_name}</h2>
-            <h5>Stage: {handleStage()}</h5>
-          </Col>
-        </Row>
-      </Space>
+      {!profile.first_name && !profile.last_name ? (
+        <Skeleton active />
+      ) : (
+        <Space align="center">
+          <Row gutter={100}>
+            <Col span={6}>
+              <img className="w-24" src={avatar} alt="avatar" />
+            </Col>
+            <Col span={18}>
+              <h2>{profile.first_name + " " + profile.last_name}</h2>
+              <h5>Stage: {handleStage()}</h5>
+            </Col>
+          </Row>
+        </Space>
+      )}
     </div>
   );
-}
+};
 
-export default ProfileBadge
+export default ProfileBadge;
