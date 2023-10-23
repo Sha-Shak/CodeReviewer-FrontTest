7dfe9c1abb3e767b39496802d8348daa908e3381
Samiya Kazi <samiya.kazi09@gmail.com>
2023-10-23 22:56:35 +0600

    feat: added login page reroute

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,8 +1,27 @@
 import { GithubOutlined } from '@ant-design/icons'
 import { Button } from 'antd'
 import conf from '../config'
+import { useEffect } from 'react'
+import { checkIfStaff } from '../Services/githubAuth.service'
+import { useNavigate } from 'react-router-dom'
 
 function LoginPage() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function checkStaff () {
+      const token = localStorage.getItem('github-access-token');
+      if (token) {
+        const isStaff = await checkIfStaff(token);
+        if (isStaff) navigate('/leads')
+      }
+    }
+
+    checkStaff();
+  }, [])
+  
+
   return (
     <div className='login-container'>
       <a href={'https://github.com/login/oauth/authorize?client_id=' + conf.GITHUB_CLIENT_ID}>
