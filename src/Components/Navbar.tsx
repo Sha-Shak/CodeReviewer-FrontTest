cb175a65495dab1308c22a0c922831227bfd68a6
Samiya Kazi <samiya.kazi09@gmail.com>
2023-11-08 17:00:48 +0600

    fix: navbar menu warning solved

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,25 +9,101 @@ import {
   UsergroupAddOutlined,
 
 } from "@ant-design/icons";
-import { Menu } from "antd";
+import { Menu, MenuProps } from "antd";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const { SubMenu } = Menu;
 
 const Navbar = () => {
   const [current, setCurrent] = useState("students");
   const navigate = useNavigate();
-
+  
   const onClick = (e: { key: string }) => {
     setCurrent(e.key);
   };
-
+  
   const logout = () => {
     localStorage.removeItem('github-access-token');
     navigate('/login');
   }
-
+  
+  const items : MenuProps['items'] = [
+    {
+      key: "dashboard",
+      icon: <DashboardOutlined />,
+      title:"Dashboard",
+      label: "Dashboard",
+      children: [
+        {
+          label: <Link to="dashboard/sourcing">Sourcing</Link>,
+          key: "sourcing",
+        },
+        {
+          label: <Link to="dashboard/training">Training</Link>,
+          key: "training",
+        }
+      ]
+    },
+    {
+      key: "candidate",
+      icon: <UsergroupAddOutlined />,
+      title: "Candidate",
+      label: "Candidate",
+      children: [
+        {
+          key: "prospects",
+          icon: <SlidersOutlined />,
+          label: <Link to="/prospects">Prospects</Link>
+        },
+        {
+          key: "deals",
+          icon: <CheckOutlined />,
+          label: <Link to="/deals">Deals</Link>
+        },
+        {
+          key: "leads",
+          icon: <UsergroupAddOutlined />,
+          label: <Link to="/leads">Leads</Link>
+        }
+      ]
+    },
+    {
+      key: "student",
+      icon: <TeamOutlined />,
+      title: "Student",
+      label: "Student",
+      children: [
+        {
+          key: "junior",
+          label: <Link to="/students/junior">Junior</Link>
+        },
+        {
+          key: "senior",
+          label: <Link to="/students/senior">Senior</Link>
+        }
+      ]
+    },
+    {
+      key: "marks",
+      icon: <NumberOutlined />,
+      label: <Link to="/softskills">Marking</Link>
+    },
+    {
+      key: "github",
+      icon: <GithubOutlined />,
+      label: "Github",
+      onClick: () => {
+        window.open("https://github.com/Project-Code-Bd");
+      }
+    },
+    {
+      key: "logout",
+      label: "Logout",
+      icon: <LogoutOutlined />,
+      danger: true,
+      onClick: logout
+    }
+  ]
   return (
     <>
       <Menu
@@ -42,56 +118,8 @@ const Navbar = () => {
         onClick={onClick}
         selectedKeys={[current]}
         mode="horizontal"
+        items={items}
       >
-        <SubMenu key="dashboard" icon={<DashboardOutlined />} title="Dashboard" >
-          <Menu.Item key="sourcing">
-            <Link to="dashboard/sourcing">Sourcing</Link>
-          </Menu.Item>
-          <Menu.Item key="training">
-            <Link to="dashboard/training">Training</Link>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="candidate"
-          icon={<UsergroupAddOutlined />}
-          title="Candidate"
-        >
-          <Menu.Item key="prospects" icon={<SlidersOutlined />}>
-            <Link to="/prospects">Prospects</Link>
-          </Menu.Item>
-          <Menu.Item key="deals" icon={<CheckOutlined />}>
-            <Link to="/deals">Deals</Link>
-          </Menu.Item>
-          <Menu.Item key="leads" icon={<UsergroupAddOutlined />}>
-            <Link to="/leads">Zendesk Leads</Link>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu key="student" icon={<TeamOutlined />} title="Student">
-          <Menu.Item key="junior">
-            <Link to="students/junior">Junior</Link>
-          </Menu.Item>
-          <Menu.Item key="senior">
-            <Link to="students/senior">Senior</Link>
-          </Menu.Item>
-        </SubMenu>
-        <Menu.Item key="marks" icon={<NumberOutlined />}>
-          <Link to="/softskills">Marking</Link>
-        </Menu.Item>
-        <Menu.Item
-          key="github"
-          icon={<GithubOutlined />}
-          onClick={() => {
-            window.open("https://github.com/Project-Code-Bd");
-          }}
-        >
-          Github
-        </Menu.Item>
-        <Menu.Item
-          key="logout"
-          icon={<LogoutOutlined />}
-          danger={true}
-          onClick={logout}
-        >Logout</Menu.Item>
       </Menu>
     </>
   );
