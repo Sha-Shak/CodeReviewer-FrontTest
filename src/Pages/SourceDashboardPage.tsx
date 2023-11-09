811360401025c51b2c86f530566ea8e3f5e41f07
sha-shak <shariar.akash@northsouth.edu>
2023-11-09 13:01:52 +0600

    add: funnel chart, modified: source dashboard page

--- src/Pages/SourceDashboardPage.tsx
+++ src/Pages/SourceDashboardPage.tsx
@@ -1,38 +1,69 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import FunnelChart from "../Components/Charts/Prospect/FunnelChart";
+import PositionChart from "../Components/Charts/Prospect/PositionChart";
 import conf from "../config";
 import { serverFetch } from "../utils/handleRequest";
-import PositionChart from "../Components/Charts/Prospect/PositionChart";
 
-export interface IPositionData {
+interface IPositionData {
   prospectId: string;
   prospectName: string;
   softSkillAverage: string;
-  HardSkillAverage: string;
+  hardSkillAverage: string;
+}
+
+export interface IFunnelData {
+  stage: string;
+  count: number;
+  name: string;
 }
 
 const SourceDashboardPage = () => {
-  const url = conf.API_BASE_URL + `/prospect/get/all/position`;
+  const positionUrl = conf.API_BASE_URL + "/prospect/get/all/position";
+  const funnelUrl = conf.API_BASE_URL + "/prospect/all/sortbystage/count";
+
   const [positionData, setPositionData] = useState<IPositionData[]>([]);
+  const [funnelData, setFunnelData] = useState<IFunnelData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await serverFetch<IPositionData[]>("get", url);
-        console.log("position data", data)
-        setPositionData(data);
+        const positionData = await serverFetch<IPositionData[]>(
+          "get",
+          positionUrl
+        );
+        setPositionData(positionData);
+
+        const funnelData = await serverFetch<Record<string, number>>(
+          "get",
+          funnelUrl
+        );
+        console.log("funnel", funnelData);
+
+        const funnelDataArray: IFunnelData[] = Object.entries(funnelData).map(
+          ([stage, count]) => ({
+            stage,
+            count,
+            name: stage,
+          })
+        );
+
+        setFunnelData(funnelDataArray);
       } catch (error) {
-        // Handle any errors here
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
-  return <div className= "flex">
-    
-   <PositionChart data={positionData} />
-  </div>
+  return (
+    <div className="flex">
+      {positionData.length > 0 && <PositionChart data={positionData} />}
+      {funnelData.length > 0 && (
+        <FunnelChart data={funnelData.filter((item) => item.count > 0).sort((item1,item2)=> item2.count - item1.count)} />
+      )}
+    </div>
+  );
 };
 
 export default SourceDashboardPage;
